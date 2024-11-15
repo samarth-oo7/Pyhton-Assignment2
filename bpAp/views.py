08450a95ff5bb97c7e5e5d615275bc2f9acae086
samarth-oo7 <132479437+samarth-oo7@users.noreply.github.com>
2024-11-15 13:02:04 -0500

    .

--- bpAp/views.py
+++ bpAp/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from django.db.models import Count
+from .models import Topic, Post
 
-# Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def home(request):
+    # Get the latest 3 published posts
+    latest_posts = Post.objects.filter(status='published').order_by('-published')[:3]
+    # Annotate topics with post counts and order by most popular topics
+    topics = (
+        Topic.objects.annotate(post_count=Count('posts'))
+        .order_by('-post_count')[:10]
+    )
+    context = {
+        'latest_posts': latest_posts,
+        'topics': topics,
+    }
+    return render(request, 'bpAp/home.html', context)
